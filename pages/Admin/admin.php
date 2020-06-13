9f696dec20015deebbc9b13b09048da3ea8070bf
peinda <62188447+peinda@users.noreply.github.com>
2020-06-13 00:30:30 +0000

    Add files via upload

---
+++ pages/Admin/admin.php
@@ -0,0 +1,48 @@
+
+<div class="row zone-connexion">
+    <div class=" zone-admin">
+      <div class="row entete">
+        <div class="col-sm-6 col-lg-6 col-6 cp">
+            <h3>CREER ET PARAMETRER DU QUIZZ</h3>
+        </div>
+        <div class="col-sm-2 btn ">
+         <button type="button" class="btn btn-primary "><a href="index.php?statut=logout">DECONNEXION</a> </button>   
+         </div>
+        <div class="col-sm-4">
+        <div id="avatar"><img src="..." ></div>
+            <div id="ab"><h3>peinda/diallo</h3>
+                         
+            </div>
+         </div>
+         </div>
+         <div class="row form-group op ">
+            <div class="col-md-3 "><button type="button" class="btn btn-link">CREER QUESTIONS</button>
+          </div>
+          <div class="col-md-3 "><button type="button" class="btn btn-link">CREER ADMIN</button>
+          </div>
+          <div class="col-md-3 "><button type="button" class="btn btn-link">LISTE QUESTIONS</button>
+          </div>
+          <div class="col-md-3 "><button type="button" class="btn btn-link">LISTE JOUEURS</button>
+          </div>
+        </div>
+        <div class="container option">
+        <?php
+            if (isset($_GET['block'])) {
+                
+                if ($_GET['block']=="creerquestion") {
+                  include ("CreerQuestion.php");
+                }
+                elseif ($_GET['block']=="inscription") {
+                    include ("inscripAdmin.php");
+                }
+                elseif ($_GET['block']=="listeJoueur") {
+                    include ("listeJoueur.php");
+                }
+                elseif ($_GET['block']=="listequestion") {
+                  include ("listequestion.php");
+                }
+            }
+        ?>
+
+      </div>
+</div>
